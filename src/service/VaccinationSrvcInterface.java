cda08a73785ee31f4fceda39e1fcbb5fa6ca4f3a
peacekedy <peaceked309@gmail.com>
2023-06-21 11:22:55 +0200

    final project client side

---
+++ src/service/VaccinationSrvcInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import model.Vaccination;
+
+/**
+ *
+ * @author IT MODERN
+ */
+public interface VaccinationSrvcInterface  extends Remote{
+    
+    public String registerVaccination(Vaccination vaccination) throws RemoteException;
+    public String updateVaccination(Vaccination vaccination) throws RemoteException;
+    public String deleteVaccination(Vaccination vaccination) throws RemoteException;
+     public List<Vaccination> allvaccination() throws RemoteException;
+}
